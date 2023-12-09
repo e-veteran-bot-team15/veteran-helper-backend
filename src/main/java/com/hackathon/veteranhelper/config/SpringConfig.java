a803bf832826ebc489975c5f1922e458803fd0af
Orest Tsiatsiak <oresttsytsyak02@gmail.com>
2023-12-09 12:01:12 +0200

    Registration form logic ready, mock for dou prepared

--- src/main/java/com/hackathon/veteranhelper/config/SpringConfig.java
+++ src/main/java/com/hackathon/veteranhelper/config/SpringConfig.java
@@ -2,6 +2,7 @@ package com.hackathon.veteranhelper.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.server.resource.authentication.DelegatingJwtGrantedAuthoritiesConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
@@ -10,21 +11,27 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringConfig {
+//    @Bean
+//    SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+//
+//        DelegatingJwtGrantedAuthoritiesConverter authoritiesConverter =
+//                // Using the delegating converter multiple converters can be combined
+//                new DelegatingJwtGrantedAuthoritiesConverter(
+//                        // First add the default converter
+//                        new JwtGrantedAuthoritiesConverter(),
+//                        // Second add our custom Keycloak specific converter
+//                        new KeycloakJwtRolesConverter());
+//
+//        // Set up http security to use the JWT converter defined above
+//        httpSecurity.oauth2ResourceServer().jwt().jwtAuthenticationConverter(
+//                jwt -> new JwtAuthenticationToken(jwt, authoritiesConverter.convert(jwt)));
+//
+//        return httpSecurity..build();
+//    }
+
     @Bean
-    SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-
-        DelegatingJwtGrantedAuthoritiesConverter authoritiesConverter =
-                // Using the delegating converter multiple converters can be combined
-                new DelegatingJwtGrantedAuthoritiesConverter(
-                        // First add the default converter
-                        new JwtGrantedAuthoritiesConverter(),
-                        // Second add our custom Keycloak specific converter
-                        new KeycloakJwtRolesConverter());
-
-        // Set up http security to use the JWT converter defined above
-        httpSecurity.oauth2ResourceServer().jwt().jwtAuthenticationConverter(
-                jwt -> new JwtAuthenticationToken(jwt, authoritiesConverter.convert(jwt)));
-        
-        return httpSecurity.build();
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests(authorize -> authorize.anyRequest().permitAll());
+        return http.build();
     }
 }
